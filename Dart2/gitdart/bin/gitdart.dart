b376ae5f4e9177ae149ac3de36122e81924d22db
Kerugod <kelvinlol53@gmail.com>
2023-02-23 14:53:53 -0700

    variable

---
+++ Dart2/gitdart/bin/gitdart.dart
@@ -0,0 +1,7 @@
+import 'package:gitdart/gitdart.dart' as gitdart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gitdart.calculate()}!');
+  
+  int x =2;
+}
---
+++ Dart2/gitdart/test/gitdart_test.dart
@@ -0,0 +1,8 @@
+import 'package:gitdart/gitdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
